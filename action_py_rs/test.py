68ca370e2cf72a04b6f61bdf0d16ac714253ba97
rjofccs <93563397+rjofccs@users.noreply.github.com>
2022-10-25 15:30:30 +0800

    Update test.py

--- action_py_rs/test.py
+++ action_py_rs/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys, arrow, os, time, json, threading
+import sys, arrow, os, time, json, threading, pymysql
 
 now = arrow.utcnow().to('Asia/Shanghai').format('YYYY-MM-DD_HH:mm:ss')
 dire = os.path.dirname(os.path.abspath(__file__))
@@ -9,7 +9,15 @@ output_filename = os.path.join(dire, 'pass.pdf')
 
 
 def test_main():
-    pass
+    db = pymysql.connect(host='localhost',
+                         user='root',
+                         password='S1234567',
+                         database='TESTDB')
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
+    db.close()
 
 if __name__ == "__main__":
     sing_thread = threading.Thread(target=test_main)
