6471ffff5a45e95e44123891b9f4f66888df70dc
rjofccs <93563397+rjofccs@users.noreply.github.com>
2022-10-30 13:46:44 +0800

    Update recog.py

--- recog.py
+++ recog.py
@@ -32,9 +32,10 @@
 	}]
 }
 '''
-import sys, arrow, os, time, datetime, json, base64, threading
+import sys, arrow, os, time, datetime, json, base64, threading, requests
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.request import CommonRequest
+from urllib.parse import urlparse
 
 dire = os.path.dirname(os.path.abspath(__file__))
 now = arrow.utcnow().to('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')
@@ -45,7 +46,7 @@ def intToStr(t):
     # return '00'+ss.strftime("%Y-%m-%d %H:%M:%S.%f")[13:-3]
 	
 
-def fileTrans(akId, akSecret, appKey, fileLink, fileName):
+def fileTrans(akId, akSecret, appKey, fileLink):
     client = AcsClient(akId, akSecret, "cn-shanghai")
 
     postRequest = CommonRequest()
@@ -57,19 +58,19 @@ def fileTrans(akId, akSecret, appKey, fileLink, fileName):
     task = {"appkey" : appKey, "file_link" : fileLink, "version" : "4.0", "enable_words" : True, "enable_sample_rate_adaptive": True}
     postRequest.add_body_params("Task", json.dumps(task))
     taskId = ""
-    try :
+    try:
         postResponse = client.do_action_with_exception(postRequest)
         postResponse = json.loads(postResponse)
-        print (postResponse)
+        # print(postResponse)
         statusText = postResponse["StatusText"]
-        if statusText == "SUCCESS" :
-            print ("录音文件识别请求成功响应！")
+        if statusText == "SUCCESS":
+            print("录音文件识别请求成功响应！")
             taskId = postResponse["TaskId"]
-        else :
-            print ("录音文件识别请求失败！")
+        else:
+            print("录音文件识别请求失败！")
             return
     except Exception as e:
-        print (e)
+        print(e)
 
     getRequest = CommonRequest()
     getRequest.set_domain("filetrans.cn-shanghai.aliyuncs.com")
@@ -83,33 +84,37 @@ def fileTrans(akId, akSecret, appKey, fileLink, fileName):
         try :
             resp = client.do_action_with_exception(getRequest)
             resp = json.loads(resp)
-            print (resp)
+            # print(resp)
             statusText = resp["StatusText"]
             if statusText == "RUNNING" or statusText == "QUEUEING" :
                 time.sleep(10)
             else :
                 break
         except Exception as e:
-            print (e)
+            print(e)
     if statusText == "SUCCESS" :
         print ("录音文件识别成功！")
         # res = resp['Result']
         # f.write(json.dumps(res))
         
-        f = open('/usr/local/www/html/'+fileName,'w')
         sens = resp['Result']['Sentences']
+        res = []
         for sen in sens:
             if sen['ChannelId']==0:
                 begin = intToStr(int(sen['BeginTime']))
                 end = intToStr(int(sen['EndTime']))
                 # f.writelines('{}--{}=={}\n'.format(begin,end,sen['Text']))
-                f.writelines('{}#t={},{}?sen={}\n'.format(fileLink,begin,end,sen['Text']))
+                # f.writelines('{}#t={},{}?sen={}\n'.format(fileLink,begin,end,sen['Text']))
+                res.append(['#t={},{}'.format(begin,end),sen['Text']])
+        return res	
     else :
-        print ("录音文件识别失败！")
-
+        print("录音文件识别失败！")
 
 
 
 appKey = "O7PwMh9AZEdSsMVt"
-fileLink = "https://files.51voa.cn/202210/meta-demonstrates-ai-powered-speech-to-speech-translation-system.mp3"
-fileTrans(accessKeyId, accessKeySecret, appKey, fileLink, 'test.txt')
+fileLink = "https://files.51voa.cn/201903/china-launches-tech-hub-megalopolis.mp3"
+fileName = os.path.basename(urlparse(fileLink).path)
+res = fileTrans(accessKeyId, accessKeySecret, appKey, fileLink)
+open('/usr/local/www/html/'+fileName.replace('.mp3','.json'),'w').write(json.dumps(res))
+open('/usr/local/www/html/'+fileName,'wb').write(requests.get(fileLink).content)
