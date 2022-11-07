48652892a27a066df6ba394d3c9233694bb8e31b
rjofccs <93563397+rjofccs@users.noreply.github.com>
2022-11-07 11:43:47 +0800

    Update voa.py

--- voa.py
+++ voa.py
@@ -2,36 +2,45 @@
 # -*- coding: utf-8 -*-
 import sys, arrow, os, datetime, json, requests, threading, urllib3
 from lxml import etree
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# selenium webdriver_manager 
 
 now = arrow.utcnow().to('Asia/Shanghai').format('YYYY-MM-DD_HH:mm:ss')
 dire = os.path.dirname(os.path.abspath(__file__))
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-output_filename = os.path.join(dire, '/tmp/')
-if not os.path.exists(output_filename):
-    os.makedirs(output_filename)
+output_filename = os.path.join(dire, '/mp3/')
+# if not os.path.exists(output_filename):
+#     os.makedirs(output_filename)
 
-
+def d(durl):
+    open(output_filename+'/'+os.path.basename(urlparse(durl)).path),'wb').write(requests.get(durl).content)
+    
 if __name__ == "__main__":
-url = 'https://www.51voa.com/'
-header = {'User-Agent': "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"}
-resp = requests.get(url, headers=header)
-html = etree.HTML(resp.text)
-xs = html.xpath("//div[@class='list']/ul/li")
-for x in xs:
-    a = x.xpath("a[last()]")[0]
-    href = a.xpath("@href")[0]
-    titl = a.xpath("text()")[0]
-    cont = requests.get(url + href, headers=header)
-    con = etree.HTML(cont.text)
-    mp3 = con.xpath("//a[@id='mp3']/@href")
-    lrc = con.xpath("//a[@id='lrc']/@href")
-    print([titl, mp3[0] if len(mp3)!=0 else '', lrc[0] if len(lrc)!=0 else ''])
+    url = 'https://www.51voa.com/'
+    header = {'User-Agent': "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"}
+    resp = requests.get(url, headers=header)
+    html = etree.HTML(resp.text)
+    xs = html.xpath("//div[@class='list']/ul/li")
+    for x in xs:
+        l = x.xpath("a[@class='lrc']")
+        if len(l) == 1:
+            a = x.xpath("a[last()]")[0]
+            href = a.xpath("@href")[0]
+            titl = a.xpath("text()")[0]
+            cont = requests.get(url + href, headers=header)
+            con = etree.HTML(cont.text)
+            mp3 = con.xpath("//a[@id='mp3']/@href")
+            lrc = con.xpath("//a[@id='lrc']/@href")
+            print(titl)
+            if len(mp3) != 0:
+                durl = mp3[0]
+                d(durl)
+            if len(lrc) != 0:
+                durl = url+lrc[0]
+                d(durl)
 
 
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# selenium webdriver_manager 
 # options = webdriver.ChromeOptions()
 # options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
 # options.add_argument('window-size=1920x3000') # 指定浏览器分辨率
